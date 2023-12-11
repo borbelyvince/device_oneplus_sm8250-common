97b94e15c078ce1a1fa0fc154f23be40457d58e5
Christos Athanasiadis <christos.ath4@gmail.com>
2023-12-11 11:21:58 +0200

    sm8250-common: Switch to HWC based brightness control

--- common.mk
+++ common.mk
@@ -74,6 +74,10 @@ PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
 
+# Display
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.sf.force_hwc_brightness=1
+
 # Doze
 PRODUCT_PACKAGES += \
     ParanoidDoze
